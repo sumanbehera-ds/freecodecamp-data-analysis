b5062183b597928a0aceae075a4dd99670745a96
SUMAN BEHERA <beherasuman44444@gmail.com>
2026-05-23 20:44:09 +0530

    Add page view time series visualizer

---
+++ page-view-time-series-visualizer/time_series_visualizer.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("fcc-forum-pageviews.csv", index_col="date", parse_dates=True)
+
+df = df[
+    (df["value"] >= df["value"].quantile(0.025))
+    & (df["value"] <= df["value"].quantile(0.975))
+]
+
+
+def draw_line_plot():
+    fig, ax = plt.subplots(figsize=(15, 5))
+    ax.plot(df.index, df["value"])
+    ax.set_title("Daily freeCodeCamp Forum Page Views 5/2016-12/2019")
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Page Views")
+    fig.savefig("line_plot.png")
+    return fig
+
+
+def draw_bar_plot():
+    df_bar = df.copy()
+    df_bar["year"] = df_bar.index.year
+    df_bar["month"] = df_bar.index.month_name()
+
+    fig = df_bar.groupby(["year", "month"])["value"].mean().unstack().plot(kind="bar").figure
+    plt.xlabel("Years")
+    plt.ylabel("Average Page Views")
+    plt.legend(title="Months")
+    fig.savefig("bar_plot.png")
+    return fig
+
+
+def draw_box_plot():
+    df_box = df.copy()
+    df_box["year"] = df_box.index.year
+    df_box["month"] = df_box.index.strftime("%b")
+
+    fig, axes = plt.subplots(1, 2, figsize=(15, 5))
+    sns.boxplot(x="year", y="value", data=df_box, ax=axes[0])
+    axes[0].set_title("Year-wise Box Plot (Trend)")
+    axes[0].set_xlabel("Year")
+    axes[0].set_ylabel("Page Views")
+
+    sns.boxplot(x="month", y="value", data=df_box, ax=axes[1])
+    axes[1].set_title("Month-wise Box Plot (Seasonality)")
+    axes[1].set_xlabel("Month")
+    axes[1].set_ylabel("Page Views")
+
+    fig.savefig("box_plot.png")
+    return fig
